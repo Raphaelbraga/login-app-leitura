9838f9060774922500f929a2d5ba1b6e7c8d3133
RaphaelBraga <raphadelbraga@hotmail.com>
2022-08-30 21:47:41 -0300

    inicio formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 
 function App() {
+const [valor, setValor] = useState ({
+  Nome: '',
+  SobreNome: '',
+  Email:'',
+})
+
   return (
     <div className="App">
-    
+      <div className="formulario">
+          <form>
+            <imput className="campoNome"
+              placeholder = "nome"
+              name="nome"
+            ></imput>
+          </form>
+      </div>
+      
     </div>
   );
 }
