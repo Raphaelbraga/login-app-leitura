479f5cc33ff79b17ab66f7a56d2b3629bebf345a
RaphaelBraga <raphadelbraga@hotmail.com>
2022-09-13 19:19:32 -0300

    criado tela home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./Home";
+import "./style.css"
 
 function App() {
-const [valor, setValor] = useState ({
-  Nome: '',
-  SobreNome: '',
-  Email:'',
-})
+  return (
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+  );
+}
+
+function Login() {
+  const navigate = useNavigate();
+
+  const adminUser = {
+    email: "admin@admin.com",
+    senha: "admin"
+  }
+
+  const [nome, setNome] = useState("");
+  const [email, setemail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const logar = () => {
+    if (email === adminUser.email && senha === adminUser.senha) {
+      alert("usuario logado com sucesso");
+      navigate("/home");
+    } else {
+      alert("usuario ou senha incorretos");
+    }
+  }
 
   return (
     <div className="App">
-      <div className="formulario">
-          <form>
-            <imput className="campoNome"
-              placeholder = "nome"
-              name="nome"
-            ></imput>
-          </form>
+      <div className="Formulario">
+        <div>
+          <input className="nome"
+            placeholder="nome"
+            name="nome"
+            value={nome}
+            onChange={(event) => setNome(event.target.value)}
+          />
+        </div>
+        <div>
+          <input className="email"
+            placeholder="email"
+            name="email"
+            value={email}
+            onChange={(event) => setemail(event.target.value)}
+          />
+        </div>
+        <div>
+          <input className="senha"
+            placeholder="senha"
+            name="senha"
+            value={senha}
+            onChange={(event) => setSenha(event.target.value)}
+          />
+        </div>
+
+        <div className="botaoLogin">
+          <button onClick={logar}>Login</button>
+        </div>
+
       </div>
-      
+
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import App from "./App";
+import "./style.css"
+
+function Home() {
+
+  return (
+    <Routes>
+      <Route path="/" exact element={<Fatura />} />
+      <Route path="/App" element={<App />} /> 
+    </Routes>
+  );
+}
+
+function Fatura() {
+  const navigate = useNavigate();
+
+  const [nome, setNome] = useState("");
+  const [email, setemail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [tipo, setTipo] = useState("");
+
+  const logout = () => {
+    // ativar component alert para confirmação depois sair
+    navigate("/");
+  }
+
+  return (
+    <div className="TelaInicial">
+      <div className="Logout">
+        <button onClick={logout}>Logout</button>
+      </div>
+      <div className="Menu">
+        <div className="nome"></div>
+        <div className="nome"></div>
+        <div className="nome"></div>
+      </div>
+    </div>
+  );
+
+}
+
+
+export default Home;
